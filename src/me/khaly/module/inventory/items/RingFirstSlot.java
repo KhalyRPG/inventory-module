e03213609e7e5bab75e929df5501cdf9ad9feda5
iAtog <atogmc58vani@gmail.com>
2021-10-25 13:09:03 -0400

    created #1 ring slot

---
+++ src/me/khaly/module/inventory/items/RingFirstSlot.java
@@ -0,0 +1,18 @@
+package me.khaly.module.inventory.items;
+
+import org.bukkit.Material;
+
+import me.khaly.core.builder.ItemBuilder;
+import me.khaly.module.inventory.items.object.InventorySlot;
+
+public class RingFirstSlot extends InventorySlot {
+
+	public RingFirstSlot() {
+		super(new ItemBuilder(Material.INK_SACK, 1, (short)7) {{
+			setName("&cRanura para anillo nº 1");
+		}}, "ring_1", 9);
+
+		this.setChangableItem(true);
+	}
+
+}
