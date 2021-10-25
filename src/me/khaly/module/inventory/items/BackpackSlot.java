f72d32585aecc82c3afdd1deec3f81cbea4f4a51
iAtog <atogmc58vani@gmail.com>
2021-10-25 13:24:02 -0400

    changed backpack texture

--- src/me/khaly/module/inventory/items/BackpackSlot.java
+++ src/me/khaly/module/inventory/items/BackpackSlot.java
@@ -1,15 +1,14 @@
 package me.khaly.module.inventory.items;
 
-import org.bukkit.Material;
-
-import me.khaly.core.builder.ItemBuilder;
+import me.khaly.core.builder.SkullItemBuilder;
 import me.khaly.module.inventory.items.object.InventorySlot;
 
 public class BackpackSlot extends InventorySlot {
 
 	public BackpackSlot() {
-		super(new ItemBuilder(Material.INK_SACK, 1, (short)	7) {{
+		super(new SkullItemBuilder() {{
 			setName("&aMochila");
+			setTexture("http://textures.minecraft.net/texture/b12c0a39cf46a72c18fba307b815462d19974feee7a078643569decbf5b7");
 		}}, "backpack_1", 13, null);
 		
 		this.setAction((user, event) -> {
