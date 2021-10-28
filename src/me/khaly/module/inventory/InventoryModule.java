b06fee05d188af56de99fc614acde5a60bd8a142
iAtog <atogmc58vani@gmail.com>
2021-10-27 20:26:26 -0400

    add accessories stats and fix a LITERALLY bin

--- src/me/khaly/module/inventory/InventoryModule.java
+++ src/me/khaly/module/inventory/InventoryModule.java
@@ -1,6 +1,7 @@
 package me.khaly.module.inventory;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -16,8 +18,10 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 import me.khaly.core.api.events.UserJoinEvent;
+import me.khaly.core.enums.StatAttribute;
 import me.khaly.core.module.Module;
 import me.khaly.core.user.User;
+import me.khaly.core.user.attributes.modifier.StatModifier;
 import me.khaly.core.util.ItemUtils;
 import me.khaly.module.inventory.items.BackpackSlot;
 import me.khaly.module.inventory.items.BraceletSlot;
@@ -55,11 +59,11 @@ public class InventoryModule extends Module implements Listener {
 		return instance;
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOW)
 	public void onInventoryClick(InventoryClickEvent event) {
 		if (event.getClickedInventory() == null || event.getInventory() == null
-				|| event.getAction() == InventoryAction.NOTHING || event.isCancelled()
-				|| event.getClickedInventory().getType() != InventoryType.PLAYER) {
+				|| event.getAction() == InventoryAction.NOTHING || event.getAction() == InventoryAction.CLONE_STACK
+				|| event.isCancelled() || event.getClickedInventory().getType() != InventoryType.PLAYER) {
 			return;
 		}
 
@@ -77,39 +81,38 @@ public class InventoryModule extends Module implements Listener {
 			ItemStack cursor = event.getCursor();
 			ItemStack item = inventory.getItem(inventorySlot.getInventorySlot());
 
-			if (!inventorySlot.isDefaultItem(item)) {
-				event.getWhoClicked().setItemOnCursor(item);
-				inventory.setItem(slot, inventorySlot.getDefaultItemStack());
-			} else {
-				if ((cursor == null || cursor.getType() == Material.AIR) && inventorySlot.isDefaultItem(item)) {
-					event.setCancelled(true);
-				} else {
-					if (inventorySlot.isDefaultItem(item) && cursor != null) {
-						if (!ItemUtils.isType(cursor, inventorySlot.getType().getType())) {
-							event.setCancelled(true);
-							return;
-						}
-
-						inventory.setItem(slot, cursor);
-						player.setItemOnCursor(null);
-					} else if (!inventorySlot.isDefaultItem(item) && cursor == null) {
-						player.setItemOnCursor(item);
-						inventory.setItem(slot, inventorySlot.getDefaultItemStack());
-						inventorySlot.onRemove().accept(user, item);
-					} else if (!inventorySlot.isDefaultItem(item) && cursor != null) {
-						if (!ItemUtils.isType(cursor, inventorySlot.getType().getType())) {
-							event.setCancelled(true);
-							return;
-						}
-
-						player.setItemOnCursor(item);
-						inventory.setItem(slot, cursor);
-						inventorySlot.onRemove().accept(user, item);
-					} else {
-						player.sendMessage("§cW-what? ._.");
-					}
-				}
+			if (((cursor == null || cursor.getType() == Material.AIR) && inventorySlot.isDefaultItem(item))
+					|| !ItemUtils.isType(cursor, inventorySlot.getType().getType())) {
+				event.setCancelled(true);
+				return;
 			}
+
+			if (inventorySlot.isDefaultItem(item) && cursor != null) {
+				inventory.setItem(slot, cursor);
+				player.setItemOnCursor(null);
+			} else if (!inventorySlot.isDefaultItem(item) && cursor == null) {
+				player.setItemOnCursor(item);
+				inventory.setItem(slot, inventorySlot.getDefaultItemStack());
+				inventorySlot.onRemove().accept(user, item);
+			} else if (!inventorySlot.isDefaultItem(item) && cursor != null) {
+				if (!ItemUtils.isType(cursor, inventorySlot.getType().getType())
+						|| !ItemUtils.isType(item, inventorySlot.getType().getType())) {
+
+					return;
+				}
+
+				player.setItemOnCursor(item);
+				inventory.setItem(slot, cursor);
+				inventorySlot.onRemove().accept(user, item);
+			} else {
+				player.sendMessage("§cW-what? ._.");
+				event.setCancelled(true);
+			}
+			/*
+			 * if (!inventorySlot.isDefaultItem(item)) {
+			 * event.getWhoClicked().setItemOnCursor(item); inventory.setItem(slot,
+			 * inventorySlot.getDefaultItemStack()); }
+			 */
 		}
 
 		event.setCancelled(inventorySlot.onAction().test(user, event));
@@ -128,5 +131,28 @@ public class InventoryModule extends Module implements Listener {
 				inventory.setItem(slot, inventorySlot.getDefaultItemStack());
 			}
 		}
+
+		setupModifiers(event.getUser(), StatAttribute.values());
 	}
+
+	private void setupModifiers(User user, StatAttribute... attributes) {
+
+		for (StatAttribute attribute : attributes) {
+			StatModifier modifier = new StatModifier("accessories", (value) -> {
+				Collection<InventorySlot> slots = getItems().values();
+				double amplifier = 0;
+
+				for (InventorySlot slot : slots) {
+					ItemStack item = user.getBukkitPlayer().getInventory().getItem(slot.getInventorySlot());
+					amplifier += ItemUtils.getModifier(attribute, item);
+				}
+
+				value.set(amplifier);
+			});
+
+			user.getAttribute(attribute).addModifier(modifier);
+		}
+
+	}
+
 }
